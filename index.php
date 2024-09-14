188450b97a59832e8b04550b42eb23204fb9a147
Pedro Eduardo dos Santos <108952479+iampedroeduardo@users.noreply.github.com>
2024-09-14 01:09:34 -0300

    pagina principal css menos informações adicionais e footer
    Co-authored-by: Jesss <JVillasBoas@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,13 +7,8 @@
 </head>
 <body>
     <div>
+        <img src="img/logo.png" alt="Logo" class="logo">
         <div class="banner-index">
-            <div class="logo">
-                <span class="simbolo"><</span>
-                <span class="simbolo">/</span>
-                <span class="titulo">Informática</span>
-                <span class="simbolo">></span>
-            </div>
             <div class="titulo-banner-div">
                 <span class="subtitulo-banner">Conhecendo o curso de</span>
                 <span class="fonte-destaque titulo-banner">Informática para internet</span>
@@ -21,41 +16,102 @@
         </div>
         <div id="links-index">
             <div class="link-index">
-                <div class="icone-link">teste</div>
+                <div class="icone-link">
+                    <img src="img/projetos.png" alt="Projetos">
+                </div>
                 <span class="link-texto">Projetos</span>
-                <span>
+                <span class="link-texto2">
                     Veja a produção científica do curso através de projetos dos estudantes
                 </span>
                 <button class="botao-index">Saiba mais</button>
             </div>
             <div class="link-index">
-                <div class="icone-link">teste</div>
-                <span class="link-texto">Docentes</span>
-                <span>
+            <div class="icone-link">
+                    <img src="img/docentes.png" alt="Docentes">
+                </div>
+                <span class="link-texto">Docentes Técnicos</span>
+                <span class="link-texto2">
                     Conheça os profissionais que tornam esse curso possível
                 </span>
                 <button class="botao-index">Saiba mais</button>
             </div>
             <div class="link-index">
-                <div class="icone-link">teste</div>
-                <span class="link-texto">Destaque</span>
-                <span>
+                <div class="icone-link">
+                    <img src="img/destaques.png" alt="Destaques">
+                </div>
+                <span class="link-texto">Destaques</span>
+                <span class="link-texto2">
                     Veja os alunos que se destacaram durante a sua trajetória no IFRS
                 </span>
                 <button class="botao-index">Saiba mais</button>
             </div>
             <div class="link-index">
-                <div class="icone-link">teste</div>
-                <span class="link-texto">Disciplina</span>
-                <span>
-                    Descubra tudo que você vai aprender ao decorrer destes anos
+                <div class="icone-link">
+                    <img src="img/disciplinas.png" alt="Disciplinas">
+                </div>
+                <span class="link-texto">Disciplinas Técnicas</span>
+                <span class="link-texto2">
+                    Descubra tudo que você vai aprender ao decorrer destes quatro anos
                 </span>
                 <button class="botao-index">Saiba mais</button>
             </div>
         </div>
     </div>
+    <div id="definicao-info-div">
+        <div id="definicao-info-pergunta">
+            <span class="definicao-info-pergunta1">Mas o que é</span>
+            <span class="definicao-info-pergunta2">Informática para Internet ?</span>
+        </div>
+        <div id="definicao-info-resposta">
+            <span id="definicao-info-resposta1">
+                Informática para Internet é um dos cursos técnicos integrados que o IFRS - Campus Rio Grande oferece. Com ele, os estudantes aprendem tudo que é necessário para ser um desenvolvedor web, incluindo desde o design e a parte estética do site até o que acontece por trás. O aluno de Informática também aprende noções de computação e como funciona o processo de criação de uma aplicação.
+            </span>
+        </div>
+    </div>
 
-        
+    <div id="areas-info">
+        <span id="areas-titulo">Um Técnico em Informática para Internet pode trabalhar em várias áreas</span>
+        <div id="areas-info-div">
+            <div class="area">
+                <div class="linha"></div>
+                <div class="area-2">
+                    <span class="titulo-area">Frontend</span>
+                    <span>
+                        De forma geral, o Frontend compreende a parte visual de sites e aplicações. Ou seja, a área das páginas em que as pessoas podem interagir. Isso significa que o Front-end se relaciona com as partes dos sites que mais se aproximam da pessoa usuária.
+                    </span>
+                </div>
+            </div>
+            <div class="area">
+                <div class="linha"></div>
+                <div class="area-2">
+                    <span class="titulo-area">Backend</span>
+                    <span>
+                        O Backend é uma parte da aplicação que tem duas responsabilidades principais: executar lógicas mais complexas e armazenar os dados da aplicação. Mas o que são essas lógicas? Basicamente, são as ações que a aplicação deve executar.
+                    </span>
+                </div>
+                
+            </div>
+            <div class="area">
+                <div class="linha"></div>
+                <div class="area-2">
+                    <span class="titulo-area">Engenharia de Software</span>
+                    <span>
+                        O engenheiro de software atua no projeto e desenvolvimento de sistemas, aplicativos e programas. Trabalha na criação do conjunto de instruções lógicas, conhecido como software, que direciona as operações de um computador ou sistema de processamento de dados.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="info-add">
+        <div class="titulo-info-add">
+            <span>Informações Adicionais</span>
+            <div class="linha-info-add"></div>
+        </div>
+        <div class="texto-info-add">
+        O curso Técnico em Informática para Internet, integrado ao Ensino Médio, é uma excelente oportunidade para quem deseja ingressar na área de tecnologia desde cedo. Com um total de 3.493 horas distribuídas ao longo de quatro anos, o curso tem uma estrutura anual e regime integral, com aulas nos períodos da manhã e tarde. Voltado para estudantes que já concluíram o Ensino Fundamental, o ingresso é feito por meio de um teste classificatório, o que garante uma seleção justa e baseada no desempenho dos 30 candidatos.
+        </div>
+    </div>
     </div>
 </body>
 </html>
