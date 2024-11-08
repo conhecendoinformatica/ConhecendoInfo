a4baba50843634ab930458e2544b9e1db8f05f4a
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2024-11-07 22:12:59 -0300

    teste bd site aleatorio Daniil

--- api/Conexao.php
+++ api/Conexao.php
@@ -1,32 +1,17 @@
 <?php
-class Conexao {
-   
-   public static $instance;
 
-   private function __construct() {
-       //
-   }
+$host = $_ENV['PG_HOST'];
+$port = $_ENV['PG_PORT'];
+$db = $_ENV['PG_DB'];
+$user = $_ENV['PG_USER'];
+$password = $_ENV['PG_PASSWORD'];
+$endpoint = $_ENV['PG_ENDPOINT'];
 
-   public static function getConexao() {
-       if (!isset(self::$instance)) {
-           self::$instance = new PDO("pgsql:host=ep-icy-mountain-a4z390r0-pooler.us-east-1.aws.neon.tech;dbname=verceldb", "default", "92DyqdeouPBl");
-           self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
-       }
+$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
 
-       return self::$instance;
-   }
+$dbconn = pg_connect($connection_string);
 
+if (!$dbconn) {
+    die("Connection failed: " . pg_last_error());
 }
-try {
-    $sql = "SELECT * FROM membros";
-    $result = Conexao::getConexao()->query($sql);
-    $lista = $result->fetchAll(PDO::FETCH_ASSOC);
-    $f_lista = array();
-    foreach ($lista as $l) {
-        $f_lista[] = $this->listaUsuarios($l);
-    }
-    var_dump($f_lista);
-} catch (Exception $e) {
-    print "Ocorreu um erro ao tentar Buscar Todos." . $e;
-}
+echo "Connected successfully";
